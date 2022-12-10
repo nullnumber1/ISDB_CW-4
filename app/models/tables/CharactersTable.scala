5ad4f839c3da7e3d7e93df9e9693d61a71e0d872
nullnumber1 <artrom1511@gmail.com>
2022-12-10 20:35:00 +0300

    Changed field to match the one in DB

--- app/models/tables/CharactersTable.scala
+++ app/models/tables/CharactersTable.scala
@@ -9,16 +9,16 @@ import slick.jdbc.H2Profile.api._
  *
  * @param tag The table tag
  */
-class Characters(tag: Tag) extends Table[Character](tag, "characters") {
-  def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def name: Rep[String] = column[String]("name")
+class CharactersTable(tag: Tag) extends Table[Character](tag, "characters") {
+  def id: Rep[Int] = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def name: Rep[String] = column[String]("name", O.Unique)
   def moving: Rep[Int] = column[Int]("moving")
   def fight: Rep[Int] = column[Int]("fight")
   def firingRange: Rep[Int] = column[Int]("firing_range")
   def * : ProvenShape[Character] = (id, name, moving, fight, firingRange) <> ((Character.apply _).tupled, Character.unapply)
 }
 
-object Characters extends TableQuery[Characters](new Characters(_)) {
+object CharactersTable extends TableQuery[CharactersTable](new CharactersTable(_)) {
   /**
    * Place for characters db methods
    */
