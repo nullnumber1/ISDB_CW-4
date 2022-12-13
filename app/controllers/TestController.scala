8d06c46e486593b78ce558043bc198b29ce4d5e0
nullnumber1 <artrom1511@gmail.com>
2022-12-13 14:40:08 +0300

    added functions to TestController for new DAO testing

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import models.Square
-import models.dao.SquareStackDao
+import models.dao.{SquareStackDao, UserCardsDao}
 import play.api.Logging
 import play.api.mvc._
 
@@ -9,17 +9,28 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class TestController @Inject()(dao: SquareStackDao, components: ControllerComponents)(implicit ex: ExecutionContext) extends AbstractController(components) with Logging {
+class TestController @Inject()(squareStackDao: SquareStackDao, userCardsDao: UserCardsDao, components: ControllerComponents)(implicit ex: ExecutionContext) extends AbstractController(components) with Logging {
   def getAll: Action[AnyContent] = Action.async {
-    dao.getAll.map { stacks =>
+    squareStackDao.getAll.map { stacks =>
       Ok(Square.toJson(stacks))
     }
   }
 
   def getCard(id: Int): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    dao.findById(id).map {
+    squareStackDao.findById(id).map {
       case Some(card) => Ok(Square.toJson(card))
       case None => NotFound
     }
+  }
+
+  def handOutCards: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    userCardsDao.giveCards().map {
+      case true => Ok
+      case false => InternalServerError("Something went wrong")
+    }
+  }
+
+  def clearCards: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    userCardsDao.deleteAll().map(_ => Ok)
   }
 }
